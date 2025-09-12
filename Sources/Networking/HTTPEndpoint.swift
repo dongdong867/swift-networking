43aa19c401462a9fb90ef075d13d9799fe701ce5
dongdong867 <93483694+dongdong867@users.noreply.github.com>
2025-09-12 19:34:51 +0800

    feat: create http networking models

---
+++ Sources/Networking/HTTPEndpoint.swift
@@ -0,0 +1,13 @@
+//
+// HTTPEndpoint.swift
+// Utils
+//
+// Created by Dong on 9/8/25
+// Copyright © 2025 dongdong867. All rights reserved.
+//
+
+import Foundation
+
+enum HTTPEndpoint {}
+
+enum EndpointPath {}
---
+++ Sources/Networking/HTTPMethod.swift
@@ -0,0 +1,17 @@
+//
+// HTTPMethod.swift
+// Utils
+//
+// Created by Dong on 9/8/25
+// Copyright © 2025 dongdong867. All rights reserved.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case GET = "GET"
+    case POST = "POST"
+    case PUT = "PUT"
+    case DELETE = "DELETE"
+    case PATCH = "PATCH"
+}
