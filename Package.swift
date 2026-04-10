852ed194921a87d61914daafa035d34d8c5522b3
Dong <dongdong0867@gmail.com>
2026-04-10 18:26:54 +0800

    [Package] Set up SPM package structure and tooling config (#38)

--- Package.swift
+++ Package.swift
@@ -1,7 +1,46 @@
 // swift-tools-version: 6.0
 
+import CompilerPluginSupport
 import PackageDescription
 
 let package = Package(
-    name: "swift-networking"
+    name: "swift-networking",
+    platforms: [
+        .iOS(.v16),
+        .macOS(.v13),
+        .watchOS(.v9),
+        .tvOS(.v16),
+        .visionOS(.v1),
+    ],
+    products: [
+        .library(name: "Networking", targets: ["Networking"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/swiftlang/swift-syntax", "600.0.0"..<"605.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "Networking",
+            dependencies: ["NetworkingMacros"]
+        ),
+        .macro(
+            name: "NetworkingMacros",
+            dependencies: [
+                .product(name: "SwiftSyntax", package: "swift-syntax"),
+                .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+                .product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
+            ]
+        ),
+        .testTarget(
+            name: "NetworkingTests",
+            dependencies: ["Networking"]
+        ),
+        .testTarget(
+            name: "NetworkingMacroTests",
+            dependencies: [
+                "NetworkingMacros",
+                .product(name: "SwiftSyntaxMacrosTestSupport", package: "swift-syntax"),
+            ]
+        ),
+    ]
 )
---
+++ Sources/Networking/Networking.swift
@@ -0,0 +1,1 @@
+// TODO: Add Networking module public API
